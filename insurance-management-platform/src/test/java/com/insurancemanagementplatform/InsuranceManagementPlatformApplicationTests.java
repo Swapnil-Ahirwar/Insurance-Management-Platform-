fcc8dad9b81652da4f6962d17bbe628b4c3218a9
Swapnil Ahirwar <ahirwarswapnil1@gmail.com>
2023-03-31 14:12:33 +0530

    first commit

---
+++ insurance-management-platform/src/test/java/com/insurancemanagementplatform/InsuranceManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurancemanagementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
