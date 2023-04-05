4ef8be4bd6dfe5346db2e1c792c4886851cf55cc
Swapnil Ahirwar <ahirwarswapnil1@gmail.com>
2023-04-05 14:59:04 +0530

    Changed the endpoint for policies Api

--- src/main/java/com/insurancemanagementplatform/controller/InsurancePolicyApiController.java
+++ src/main/java/com/insurancemanagementplatform/controller/InsurancePolicyApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/policy")
+@RequestMapping("/api/policies")
 public class InsurancePolicyApiController {
 
     @Autowired
